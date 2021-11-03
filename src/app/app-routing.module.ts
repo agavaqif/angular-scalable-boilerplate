1b821d41f917b788286d2160c8d5d75e24a3a273
Vagif <agavaqif@yahoo.com>
2021-11-03 16:26:18 -0400

    Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    loadChildren: ()=> import('./dashboard/dashboard.module').then(m =>m.DashboardModule)
+  },
+  {
+    path: 'login',
+    loadChildren: ()=>import('./auth/auth.module').then(m =>m.AuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
